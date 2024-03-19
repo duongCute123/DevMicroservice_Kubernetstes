0a3f56eb1a26985285271a2fd18453b15822d6f5
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2024-03-19 19:53:45 +0700

    add: code config server

---
+++ SpringConfigServer/src/test/java/spring/config/server/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
